f457d81a04ece28b90325239d3d38c0400e44e74
LonerDeni <124445115+LonerDeni@users.noreply.github.com>
2023-03-15 22:58:15 +0300

    add folder src

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Worker.OnTaskDoneListener listenerOne = System.out::println;
+        Worker.OnTaskErrorListener listenerTwo = System.out::println;
+        Worker worker = new Worker(listenerOne, listenerTwo);
+        worker.start();
+    }
+}
